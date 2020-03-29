f691f4eee127fc324efb94c1a4b060ba88e684b0
sridivyapemmaka <62158012+sridivyapemmaka@users.noreply.github.com>
2020-03-29 21:13:30 +0530

    Add files via upload

---
+++ computer-science.py
@@ -0,0 +1,21 @@
+#"computer-science"
+
+a="computer-science"
+print(a[2:-2:2])
+print(a[2:-2:-2])
+print(a[-2:2:2])
+print(a[-2:2:-2])
+print(a[ :5:-1])
+print(a[ :5:1])
+print(a[-5:-1])
+print(a[10: :1])
+print(a[10: :-1])
+print(a[10: :-2])
+print(a[-10:10:10])
+print(a[ : :10])
+print(a[ : :-5])
+print(a[ : :-8])
+print(a[ :-8:2])
+print(a[-5:5:2])
+print(a[-10:-1:-1])
+print(a[-9:9:-2])
