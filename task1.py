bdbc452a2d89aff1c31a031053f4feb591d7ef4f
sridivyapemmaka <62158012+sridivyapemmaka@users.noreply.github.com>
2020-04-07 19:12:54 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,61 @@
+#task1
+#create a emtydictionary
+dict1={}
+print(type(dect1))
+
+#add bellow elements
+dict1={"hello":len("hello"),"python":len("python"),"val":len("val")}
+print(dect1)
+
+"hello":5(len of key)
+python:6
+val:3
+print all key()
+print(dect1.keys())
+print all values
+print(dect1.values())
+
+#create a variable
+a="python"
+b=5
+#output text and a variable
+print(a)
+print(b)
+#add a variable to another variable
+c=b
+print(c)
+#verify the type of object
+print(type(c))
+print(type(b))
+print(type(a))
+
+#create integers
+d=5
+e=1.
+f=lr5
+g=5j
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+#python casting
+#casting-integers
+print(type(int(d)))
+print(type(float(e)))
+print(type(str(g)))
+#python string
+#get th charector at position 1 of a string
+string="python"
+print(string1[1])
+#get character from position 2 to 5
+print(string1[2:-1])
+#remove white space from begining and end of string
+sting2="java"
+string3=string.strip()
+print(string3)
+#return lenth of string
+print(len(string2))
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,8 @@
+#logical operaters and comparision operators
+x=8
+print(x >= 9)
+print(x <= 5)
+print(x > 3)
+print(x < 5)
+print(x == 3)
+print(x >= 4)
